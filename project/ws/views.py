3a9147599500305ef4e383ddd44aa60e3c67e421
Madi-S <shaiken.m@mail.ru>
2024-03-15 22:50:15 +0500

    SocketIO form example embedded

--- project/ws/views.py
+++ project/ws/views.py
@@ -1,8 +1,11 @@
 import json
-from fastapi import WebSocket
+import socketio
+from fastapi import WebSocket, FastAPI
+from socketio.asyncio_namespace import AsyncNamespace
 
 from project import broadcast
 from project.ws import ws_router
+from project.config import settings
 from project.celery_utils import get_task_info
 
 
@@ -28,3 +31,29 @@ async def update_celery_task_status(task_id: str):
         message=json.dumps(get_task_info(task_id))
     )
     await broadcast.disconnect()
+
+
+class TaskStatusNameSpace(AsyncNamespace):
+    async def on_join(self, sid, data):
+        self.enter_room(sid=sid, room=data['task_id'])
+        await self.emit('status', get_task_info(data['task_id']), room=data['task_id'])
+
+
+def register_socketio_app(app: FastAPI):
+    mgr = socketio.AsyncRedisManager(settings.WS_MESSAGE_QUEUE)
+    sio = socketio.AsyncServer(
+        async_mode='asgi',
+        client_manager=mgr,
+        logger=True,
+        engineio_logger=True
+    )
+    sio.register_namespace(TaskStatusNameSpace('/task_status'))
+    asgi = socketio.ASGIApp(socketio_server=sio)
+    app.mount('/ws', asgi)
+
+
+def update_celery_task_status_socketio(task_id):
+    external_sio = socketio.RedisManager(
+        settings.WS_MESSAGE_QUEUE, write_only=True)
+    external_sio.emit('status', get_task_info(task_id),
+                      room=task_id, namespace='/task_status')
